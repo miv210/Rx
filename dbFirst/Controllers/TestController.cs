c97a9a38d8018759ca29404854dfc0cd5b98d9e9
RxGroup <voronok309@mail.ru>
2023-02-17 16:13:20 +0700

    updateTestController

--- dbFirst/Controllers/TestController.cs
+++ dbFirst/Controllers/TestController.cs
@@ -10,62 +10,61 @@ namespace dbFirst.Controllers
     {
         dbWorker db = new dbWorker();
 
-        [HttpGet("getCountAirport")]
+        [HttpGet("airport/count")]
         public async Task<ActionResult<int>> GetCountAirport()
         {
             var countAirport =  db.GetCountAirport();
             return countAirport;
         }
-
-        [HttpGet("GetCountTicketAirport/{id:minlength(3)}")]
-        public async Task<ActionResult<int>> GetCountTicketAirport(string id)
-        {
-            var coutTicket = db.GetCountTicketAirport(id);
-            return coutTicket;
-        }
-
-        [HttpPost("AddAirport/{name}")]
+        [HttpPost("airport/{name}")]
         public async Task<ActionResult> AddAirport(string name)
         {
             db.AddAirport(name);
             return new OkResult();
         }
 
-        [HttpPost("addFlight")]
-        public async Task<ActionResult> AddFlihgt()
-        {
-            
-            db.AddFligt("LED");
-            return new OkResult();
-        }
 
-        [HttpPost("UpdateAircraft/{name}")]
-        public async Task<ActionResult> UpdateAircraft(string name)
+        [HttpGet("ticket/count/airport/{id:minlength(3)}")]
+        public async Task<ActionResult<int>> GetCountTicketAirport(string id)
         {
-            db.UpdateAircraft(name);
-            return new OkResult();
+            var coutTicket = db.GetCountTicketAirport(id);
+            return coutTicket;
         }
-
-        [HttpPost("SetStatus/{statusName}")]
-        public async Task<ActionResult> SetStatus(string statusName)
-        {
-            db.SetStatus(statusName);
-            return new OkResult();
-        }
-
-        [HttpGet("SerchName")]
+        [HttpGet("ticket/flighted")]
         public async Task<ActionResult<IEnumerable<PassengFl4>>> SerchName()
         {
             var serch = db.SerchName();
             return serch;
         }
 
-        [HttpGet("CountFlights")]
+
+        [HttpPost("flight/add")]
+        public async Task<ActionResult> AddFlihgt()
+        {
+            
+            db.AddFligt("LED");
+            return new OkResult();
+        }
+        [HttpPut("flight/setstatus/{statusName}")]
+        public async Task<ActionResult> SetStatus(string statusName)
+        {
+            db.SetStatus(statusName);
+            return new OkResult();
+        }
+        [HttpGet("flight/count")]
         public async Task<ActionResult<IEnumerable<CountFli>>> CountFlights()
         {
             var counFl = db.CountFlights();
 
             return counFl;
+        }
+
+
+        [HttpPut("aircraft/update/{name}")]
+        public async Task<ActionResult> UpdateAircraft(string name)
+        {
+            db.UpdateAircraft(name);
+            return new OkResult();
         }
     }
 }
